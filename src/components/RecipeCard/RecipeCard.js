b1007a50fab511197fa277b8337e0e340fa78b8b
brenley-woodard <brenleyew@gmail.com>
2023-03-06 14:04:49 -0800

    created recipe cards that are rendering to page

--- src/components/RecipeCard/RecipeCard.js
+++ src/components/RecipeCard/RecipeCard.js
@@ -1,8 +1,14 @@
 import React from 'react';
 import './RecipeCard.css';
 
-export default function RecipeCard() {
+export default function RecipeCard({ name, type, season, have_made, expectations }) {
   return (
-    <div>RecipeCard</div>
+    <div>
+      <h2>{name}</h2>
+      <p>{type}</p>
+      <p>{season}</p>
+      <p>{have_made}</p>
+      <p>{expectations}</p>
+    </div>
   );
 }
